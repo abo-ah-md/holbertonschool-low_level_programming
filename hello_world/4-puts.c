355d875b68cee4c4adbd3c9dc022995c793320ae
abo-ah-md <a.alshebel.dev@gmail.com>
2025-04-25 08:54:14 +0000

    fixed the code for betty-style compliance

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-*/This main function uses the puts function to return a string to standerd output */
+/*This main function uses the puts function to return a string to standerd output */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
