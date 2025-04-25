789b1bc93c41031578e907f6fa076596ae6be48e
abo-ah-md <a.alshebel.dev@gmail.com>
2025-04-25 11:29:54 +0000

    quick fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,10 +5,10 @@
 */
 int main(void)
 {
-  printf("Size of a char:",sizeof(char), "byte(s)");
- printf("Size of a int:",sizeof(int)"byte(s)");
- printf("Size of a long int:", sizeof(long)"byte(s)");
-printf("Size of a long long int:"sizeof(long * 2)"byte(s)");
-printf("Size of a float:"sizeof(float)"byte(s)");
+printf("Size of a char:%zu byte(s)", sizeof(char));
+printf("Size of a int:%zu byte(s)", sizeof(int));
+printf("Size of a long int:%zu byte(s)", sizeof(long));
+printf("Size of a long long int:%zu byte(s)", sizeof(long long int));
+printf("Size of a float:%zu byte(s)", sizeof(float));
 return (0);
 }
