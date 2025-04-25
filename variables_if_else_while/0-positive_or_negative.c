26d7d9fdaef4815ec9c466c350ba582c77ea06ad
abo-ah-md <a.alshebel.dev@gmail.com>
2025-04-25 15:24:26 +0000

    added a c file that generates a random number and output it's value - or + or 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 if (n < 0)
 {
-printf("is positive")
+printf ("is positive");
 }
 else
 {
 if (n == 0)
 {
-printf("is zero")
+printf("is zero");
 }
 else
 printf("is negative");
