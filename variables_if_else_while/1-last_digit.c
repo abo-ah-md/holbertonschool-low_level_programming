4f39f9723dfafd17fc45e8c873dae1a3fdd54e42
abo-ah-md <a.alshebel.dev@gmail.com>
2025-04-25 16:09:28 +0000

    aa

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - generates a random number
+* Return: 0
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+
+return (0);
