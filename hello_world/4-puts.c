d3ff9e0d2111be33f0d2944cbe06f0397b91ce7f
abo-ah-md <a.alshebel.dev@gmail.com>
2025-04-25 09:57:36 +0000

    betty commint fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void)/*This main function return a string to standerd output*/
+/**
+*
+*Description: onThis main function return a string to standerd output
+*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
