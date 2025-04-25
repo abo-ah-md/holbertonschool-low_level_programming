e3a097d73173ed967904a3fd5967fd9d78e57037
abo-ah-md <a.alshebel.dev@gmail.com>
2025-04-25 23:13:14 +0000

    emacs config

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,6 +12,10 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+int last_number = n % 10;
 /* your code goes there */
-
+if(n > 5) printf("Last digit of %d is %d and is greater than 5\n",n,last_number);
+if(n == 0) printf("Last digit of %d is %d and is 0\n",n , last_number);
+if(n < 6 && n != 0) printf("Last digit of %d is %d and is less than 6 and not 0\n",n,last_number);
 return (0);
+}
