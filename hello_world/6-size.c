3e19d8c9aaca37562e649416074f1f9401066588
abo-ah-md <a.alshebel.dev@gmail.com>
2025-04-25 11:08:54 +0000

    ss

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - prints the size of datatypes in the system to standerd output
+* Return: 0
+*/
+int main(void)
+{
+printf("Size of a char:", sizeof(char)"byte(s)");
+printf("Size of a int:", sizeof(int)"byte(s)");
+printf("Size of a long int:", sizeof(long)"byte(s)");
+printf("Size of a long long int:", sizeof(long * 2)"byte(s)");
+printf("Size of a float:", sizeof(float)"byte(s)");
+return (0);
+}
