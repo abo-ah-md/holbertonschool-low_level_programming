5cefcf7755044739f4b18c801ad02041f8b77258
abo-ah-md <abo-ah-md1@hotmail.com>
2025-04-26 11:39:15 +0000

    q

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - generate random number and
+* prints the numbers from 0 to 10
+* in lowercase and form a to f, followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
