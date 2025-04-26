cf48c6d07c830d7795ed1af9ba5c29906a2dbd00
abo-ah-md <abo-ah-md1@hotmail.com>
2025-04-26 10:46:19 +0000

    a

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -10,10 +10,16 @@
 int main(void)
 {
 char n;
-for (n = '0'; n <= '10'; n++)
+for (n = 48; n <= 57; n++)
 {
 putchar(n);
 }
+if (n != 58)
+{
+putchar(49);
+putchar(48);
+}
+
 putchar('\n');
 
 return (0);
