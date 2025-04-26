59373ae58086ec2d113dedcf89d0b6eda8c954e2
abo-ah-md <abo-ah-md1@hotmail.com>
2025-04-26 14:24:56 +0000

    a

---
+++ functions_nested_loops/print_alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+char alphabet[] = "abcdefghijclmnopqrstuvwxyz"
+int i;
+while(alphabet[i] !="0/" )
+{
+Putchar(alphabet[i]);
+i++
+}
+}
