fed8bcbdd3ac5e2bb36df4c281642e84e0868fb2
abo-ah-md <abo-ah-md1@hotmail.com>
2025-04-26 10:24:44 +0000

    adding putchar c that print alphabiet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - generate random number and
+* prints the alphabet in lowercase, followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+char c;
+char C;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (C = 'A';C <= 'Z';C++)
+putchar('\n');
+return (0);
+}
