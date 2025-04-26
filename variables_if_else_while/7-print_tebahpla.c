e6c1c5ae436cc9592bb835ca48b13bb1d2603eca
abo-ah-md <abo-ah-md1@hotmail.com>
2025-04-26 10:51:49 +0000

    print all alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - generate random number and
+* prints the alphabet in lowercase, followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'z'; c <= 'a'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+
+return (0);
+}
