49bf1c95bd8f0e9d84eeb38e2066dae53cd09acf
abo-ah-md <abo-ah-md1@hotmail.com>
2025-04-26 10:14:17 +0000

    aa

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - generate random number and
+ prints the alphabet in lowercase, followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+for (char c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
