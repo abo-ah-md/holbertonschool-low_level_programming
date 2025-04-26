1a889a95a92124b7932902dd66e235053bb1ad9e
abo-ah-md <abo-ah-md1@hotmail.com>
2025-04-26 11:03:21 +0000

    a

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - generate random number and
+* prints the numbers from 0 to 10
+* in lowercase and form a to f, followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+char n;
+char alpha;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (alpha = 97; alpha <= 102; alpha++)
+{
+putchar(alpha)
+}
+putchar('\n');
+
+return (0);
+}
