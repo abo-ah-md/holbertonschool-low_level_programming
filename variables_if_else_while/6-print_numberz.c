9f3259ab1cb9fa77ac92e9f127557366d33084ac
abo-ah-md <abo-ah-md1@hotmail.com>
2025-04-26 10:49:46 +0000

    c file that prints 10 base numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - generate random number and
+* prints the numbers from 0 to 10
+* in lowercase, followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+char n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+putchar('\n');
+
+return (0);
+}
