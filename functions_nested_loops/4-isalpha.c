a8e3e367697d98eb562d6136a329c157aa4ae2b3
abo-ah-md <abo-ah-md1@hotmail.com>
2025-04-27 21:10:01 +0000

    a

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -8,12 +8,14 @@
 int _isalpha(int c)
 {
 
-if (c >= 'a' && c <= 'z' || c >= 'A' && c >= 'Z')
+if (c >= 'a' && c <= 'z')
 {
 return (1);
 }
-else
+if (c >= 'A' && c >= 'Z')
 {
-return (0);
+return (1);
 }
+return (0);
+
 }
