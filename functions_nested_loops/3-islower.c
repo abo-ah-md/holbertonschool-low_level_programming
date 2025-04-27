ff8d7c0624dcb34e2f2f40dcee8be3dfccaa0296
abo-ah-md <abo-ah-md1@hotmail.com>
2025-04-27 20:37:22 +0000

    q

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isLower - check and prints if the alphabet in lowercase
+*Return: 0
+*/
+int _isLower(int c)
+{
+
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+int i;
+for(i = 0;i != '\0';i++)
+{
+
+if(c == alphabet[i])
+return 1;
+}
+return 0;
+}
