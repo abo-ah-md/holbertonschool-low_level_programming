27827cec9f1c57bea3b3908b0d2e5f1596bc7703
abo-ah-md <abo-ah-md1@hotmail.com>
2025-04-27 22:09:01 +0000

    a

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_sign- prints the abdulute value of anumber number
+*@n:the number to check
+*Return: abs
+*/
+int print_sign(int n)
+{
+
+if (n < 0)
+{
+n = (-1) * n;
+}
+return (n);
+}
