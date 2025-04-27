6a2b69202dafa33099d04f74cde4bdb1d95b7606
abo-ah-md <abo-ah-md1@hotmail.com>
2025-04-27 13:53:32 +0000

    added c Script

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints the alphabet in lowercase
+*/
+void print_alphabet_x10(void)
+{
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+
+int i = 0;
+
+int ten;
+
+for (ten = 0; ten <= 10; ten++)
+{
+while (alphabet[i] != '\0')
+{
+_putchar(alphabet[i]);
+i++;
+}
+}
+}
