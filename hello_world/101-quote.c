2b1cb2b3eb394152bb0ae2146ab8b99b615854bb
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-01 17:45:06 +0000

    a

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <errno.h>
 int main() {
 
-perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 return (1);
 }
