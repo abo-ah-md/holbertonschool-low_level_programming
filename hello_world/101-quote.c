f797509fc153d793431de48ea983134e358acd27
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-01 18:32:23 +0000

    a

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,8 +6,7 @@
 int main(void)
 {
 
-const char *msg = "and that piece of art is useful\" - "
-"Dora Korpar, 2015-10-19\n";
+char *msg = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
 
 write(2, msg, 59);
 
