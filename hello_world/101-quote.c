20c957d8edcf7de7e0fef97b5bd3052f1d5d0e8b
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-01 18:28:04 +0000

    a

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,9 +1,15 @@
 #include <unistd.h>
-int main() {
+/**
+*main - return an error message
+*Return:1
+*/
+int main(void)
+{
 
-const char *msg ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const char *msg = "and that piece of art is useful\" - "
+"Dora Korpar, 2015-10-19\n";
 
-write(2,msg,59);
+write(2, msg, 59);
 
 return (1);
 }
