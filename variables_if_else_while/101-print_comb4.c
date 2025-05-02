7be460613084e682a417db35c543b1e11ffc0d28
abo-ah-md1 <abo-ah-md1@hotmail.com>
2025-05-02 10:13:06 +0000

    a

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - generate combanation of 2 digits
+*
+*
+* Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+int j;
+for (j = i + 1; j <= 9; j++)
+{
+int k;
+for(k = j + 1; k <= 9; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+
+if (i != 7)
+{
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar('\n');
+}
+}
+}
+}
+return (0);
+}
