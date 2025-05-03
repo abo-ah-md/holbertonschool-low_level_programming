855d29225edd04cfab5467f18325ca97af027a44
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-03 11:16:02 +0000

    1

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -18,9 +18,9 @@ for (j = i + 1; j <= 99; j++)
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 putchar(' ');
-putchar(j / 10+ '0');
+putchar(j / 10 + '0');
 putchar(j % 10 + '0');
-if(i  != 98)
+if (i != 98)
 {
 putchar(',');
 putchar(' ');
