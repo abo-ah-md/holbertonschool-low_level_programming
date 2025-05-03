cf913b72ae3321cf3c9ce0bab45fb0cbe2d5f7dc
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-03 11:00:20 +0000

    used % and / for itteration

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -15,16 +15,13 @@ for (i = 0; i <= 99; i++)
 int j;
 for (j = i + 1; j <= 99; j++)
 {
-putchar(i + '0');
-putchar(i + '0');
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
 putchar(' ');
-putchar(i + '0');
-putchar(j + '0');
-
+putchar(j / 10+ '0');
+putchar(j % 10 + '0');
 putchar(',');
 putchar(' ');
-
-putchar('\n');
 }
 }
 return (0);
