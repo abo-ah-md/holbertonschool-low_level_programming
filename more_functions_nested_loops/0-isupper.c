d01cb8681f66697940dbe69ba13a06a172cd3485
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-05 13:08:11 +0000

    a

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+/**
+* _isupper - check for uppercase.
+*
+* Return:  1 if uppercase else 0.
+*@c: a char
+*/
+int _isupper(int c)
+{
+if (c > 64 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
