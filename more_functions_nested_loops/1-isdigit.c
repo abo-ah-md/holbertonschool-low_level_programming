22c09c94d9130698b4128b823d717f266f3f78af
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-05 13:12:42 +0000

    q

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+* _isdigit - check for uppercase.
+*
+* Return:  1 if digit  else 0.
+*@c: a char
+*/
+int _isdigit(int c)
+{
+if (c > 47 && c < 58)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
