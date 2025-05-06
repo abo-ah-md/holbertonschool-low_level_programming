87cb55f4b972ccaf7453c74092807ee3b14484ad
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-06 05:59:06 +0000

    a

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,6 +10,11 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+continue;
+}
 if (i % 3 == 0)
 {
 printf("Fizz ");
@@ -17,10 +22,6 @@ printf("Fizz ");
 if (i % 5 == 0)
 {
 printf("Buzz ");
-}
-if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz ");
 }
 if (i % 3 != 0 && i % 5 != 0)
 {
