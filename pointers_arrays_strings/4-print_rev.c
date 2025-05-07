731e396f45b13e96f69c69df89befc1730388082
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-07 09:32:24 +0000

    a

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,8 +9,9 @@ void print_rev(char *s)
 {
 int i;
 int j;
-for (i = 0 ; s[i] != '\0'; i++);
-for (j = i-1 ; j > -1; j--)
+for (i = 0 ; s[i] != '\0'; i++)
+{}
+for (j = i - 1 ; j > -1; j--)
 {
 _putchar(s[j]);
 }
