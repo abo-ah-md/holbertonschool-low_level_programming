db4417b928239c8e620954c032c88f933cf5f416
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-07 12:39:14 +0000

    a

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *rev_string - revrese  a string
- *@s: string
- *
- */
+*rev_string - revrese  a string
+*@s: string
+*
+*/
 void rev_string(char *s)
 {
 int i;
 int j;
-int k;
+char tmp;
 for (i = 0 ; s[i] != '\0'; i++)
 {}
-char temp[i];
-for (j = 0; j < i; j++)
+i--;
+
+for (j = 0; j <  i; j++)
 {
-temp[j] = s[i - 1 - j];
-}
-for (k = 0; k < i; k++)
-{
-s[k] = temp [k];
+tmp  = s[j];
+s[j] = s[i];
+s[i] = tmp;
+i--;
 }
 }
