437c4a205d59d623f1730131d3761bff63951c05
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-09 23:36:41 +0300

    a

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,39 +8,33 @@
 *Return:copies of the str
 */
 
-
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int started = 0;
-
-    while (*s)
-    {
-        if (*s == '-' || *s == '+')
-        {
-            if (started)
-                break;
-            if (*s == '-')
-                sign *= -1;
-        }
-        else if (*s >= '0' && *s <= '9')
-        {
-            int digit = *s - '0';
-
-            // Check for overflow BEFORE it happens
-            if (result > (INT_MAX - digit) / 10)
-                return (sign == 1 ? INT_MAX : INT_MIN);
-
-            result = result * 10 + digit;
-            started = 1;
-        }
-        else if (started)
-        {
-            break;
-        }
-        s++;
-    }
-
-    return result * sign;
+int sign = 1;
+int result = 0;
+int started = 0;
+while (*s)
+{
+if (*s == '-' || *s == '+')
+{
+if (started)
+break;
+if (*s == '-')
+sign *= -1;
+}
+else if (*s >= '0' && *s <= '9')
+{
+int digit = *s - '0';
+if (result > (INT_MAX - digit) / 10)
+return (sign == 1 ? INT_MAX : INT_MIN);
+result = result * 10 + digit;
+started = 1;
+}
+else if (started)
+{
+break;
+}
+s++;
+}
+return (result *sign);
 }
