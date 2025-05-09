94c620f30b41d110a151284bfa7137f5d6f50308
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-09 21:20:08 +0300

    a

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -21,5 +21,9 @@ while (s[i] && s[i] >= '0' && s[i] <= '9')
 result = result * 10 + (s[i] - '0');
 i++;
 }
+if (result == -2147483648 && sign == -1)
+{
+return (result);
+}
 return (result *sign);
 }
