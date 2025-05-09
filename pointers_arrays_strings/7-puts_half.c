b6bea51fe33d496d58d26c150c93f96893b73b1c
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-09 16:48:54 +0300

    a

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,18 @@ void puts_half(char *str)
 {
 int i;
 int j;
+
 for (i = 0 ; str[i] != '\0'; i++)
 {}
+
 if (i % 2 != 0)
 {
 i = (i + 1);
 }
 
-
-for (j = i / 2 ; j !=  i; j++)
+for (j = i / 2 ; str[j]; j++)
 {
 _putchar(str[j]);
 }
+_putchar('\n');
 }
