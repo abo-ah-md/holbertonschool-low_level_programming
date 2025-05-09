25a0b974d07c0726b7ee785607ef91de454e0887
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-09 19:59:06 +0300

    q

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_atoi - copies n elements of an arr to another
+*@s: pointer to string
+*
+*Return:copies of the str
+*/
+int _atoi(char *s)
+{
+int result = 0;
+int neg = 1;
+int i;
+
+for (i = 0; s[i]; i++)
+{
+
+if (s[i] <= '9' && s[i] >= '0')
+{
+result = result * 10 + (s[i] - '0');
+}
+if (s[i] == '-')
+{
+if (s[i + 1] <= '9' && s[i + 1] >= '0')
+{
+neg = -1;
+}
+}
+}
+return (result *neg);
+}
