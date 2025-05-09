8ef6189bd8b21da92c6a8052fd735ee82703afe7
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-09 17:01:01 +0300

    a

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_array -prints n elements of an array of integers
+*@a: pointer_number
+*@n:number
+*/
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i != n; i++)
+{
+printf("%d", a[i]);
+printf(",");
+printf(" ");
+
+}
+printf("\n");
+}
