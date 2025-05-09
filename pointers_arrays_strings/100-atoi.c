91f3e77e0004ab60ba26ad62b22f8f9f95714eec
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-09 22:29:06 +0300

    a

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -18,16 +18,21 @@ i++;
 
 while (s[i] && s[i] >= '0' && s[i] <= '9')
 {
-
-if ((result > 214748364 || (result == 214748364 && s[i] - '0' > 8))  && sign == -1)
+ if (sign == 1)
 {
-return (-2147483648);
+if (result > 214748364 || (result == 214748364 && s[i] - '0' > 7))
+{
+  return 2147483647;
 }
 else
 {
+if (result > 214748364 || (result == 214748364 && s[i] - '0' > 8))
+return -2147483648;
+}
+}
 result = result * 10 + (s[i] - '0');
 i++;
-}
+
 }
 return (result *sign);
 }
