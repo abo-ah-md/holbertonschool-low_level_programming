6d1e05d7b5496e06808fdd4548060b5c68f8ea93
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-11 12:39:03 +0300

    a

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+**cap_string - changes all lowercase letters of a string to uppercase
+*@str: array of chars
+*Separators of words: space, tabulation, new line, ,, ;, ., !, ?, ", (, ),{,}
+* space, tabulation, new line,
+*Return: uppercased strings
+*/
+
+char *cap_string(char *str)
+{
+char alphabetUpper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alphabetLower[] = "abcdefghijklmnopqrstuvwxyz";
+char seprators[] = " \n,;.!?\"(){}\t";
+int i = 0;
+while (str[i])
+{
+int j = 0;
+while (alphabetLower[j])
+{
+if (str[i] == alphabetLower[j])
+{
+int k = 0;
+while (seprators[k])
+{
+if (str[i - 1] == seprators[k])
+{
+str[i] = alphabetUpper[j];
+}
+k++;
+}
+}
+j++;
+}
+i++;
+}
+return (str);
+}
