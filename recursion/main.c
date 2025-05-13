c4b9095aa2d9d686a2381bfed2ccb619dd4fc45a
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-14 01:48:26 +0300

    a

--- recursion/main.c
+++ recursion/main.c
@@ -9,9 +9,22 @@
 int main(void)
 {
     int r;
-    r = _sqrt_recursion(1024);
+
+    r = is_prime_number(1);
     printf("%d\n", r);
-    r = _sqrt_recursion(16777216);
+    r = is_prime_number(1024);
+    printf("%d\n", r);
+    r = is_prime_number(16);
+    printf("%d\n", r);
+    r = is_prime_number(17);
+    printf("%d\n", r);
+    r = is_prime_number(25);
+    printf("%d\n", r);
+    r = is_prime_number(-1);
+    printf("%d\n", r);
+    r = is_prime_number(113);
+    printf("%d\n", r);
+    r = is_prime_number(7919);
     printf("%d\n", r);
     return (0);
 }
