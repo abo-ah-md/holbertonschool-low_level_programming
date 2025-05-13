9ece2ffb794f8524cd82e494abcc08f74d2a04f6
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-14 00:06:20 +0300

    a

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include "main.h"
 /**
+**sqrt_helper- adding an argument for guessing the number.
 *_sqrt_recursion- returns the length of a string.
-*
-@n: number
+*@n: number
+*@guess:the sqrt guess number
 *Return: sqrt of n
 */
-int _sqrt_recursion(int n);
 int sqrt_helper(int n, int guess);
-
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int n);
 {
-    if (n < 0)
-        return -1;
-    return sqrt_helper(n, 0);
+if (n < 0)
+return (-1);
+return (sqrt_helper(n, 0));
 }
-
 int sqrt_helper(int n, int guess)
 {
-    if (guess * guess == n)
-        return guess;
-    if (guess * guess > n)
-        return -1;
-    return sqrt_helper(n, guess + 1);
+if (guess * guess == n)
+return (guess);
+if (guess * guess > n)
+return (-1);
+return (sqrt_helper(n, guess + 1));
 }
