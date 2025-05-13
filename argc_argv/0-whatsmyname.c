5364141d2b4ebe4dde72b9a46795b374e864385d
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-14 02:22:56 +0300

    s

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - check the code
+*@argc:arguments count
+*@argv:the argument array of command string
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
