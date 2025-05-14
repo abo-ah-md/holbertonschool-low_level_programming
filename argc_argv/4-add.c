0669808da675d9af28135bf82260dd41ff5f6bfb
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-14 04:14:16 +0300

    d

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,16 +2,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 /**
-* main - check the code
-*@argc:arguments count
-*@argv:the argument array of command string
-* Return: when no number in argv 0 if error or normal
-*/
-int main(int argc, char *argv[])
-{
-int i = 1;
-int result = 0;
-/**
 * isvalid - check the string if it is a number char
 *@s:pointer to string
 * Return: 0 not valid 1 if valid
@@ -26,6 +16,16 @@ s++;
 }
 return (1);
 }
+/**
+* main - check the code
+*@argc:arguments count
+*@argv:the argument array of command string
+* Return: when no number in argv 0 if error or normal
+*/
+int main(int argc, char *argv[])
+{
+int i = 1;
+int result = 0;
 
 if (argc == 1)
 {
