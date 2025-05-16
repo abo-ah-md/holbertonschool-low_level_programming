75601846a0051c3bec35fc032bdee4be736b2d37
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-16 15:00:06 +0300

    a

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - main function
+*Return: always return 0
+*/
+
+int main(void)
+{
+long fib0 = 1;
+long fib1 = 2;
+long newfib = 0;
+long sumof_even_fib = 0;
+while (newfib <= 4000000)
+{
+newfib = fib0 + fib1;
+fib0 = fib1;
+fib1 = newfib;
+if (newfib % 2 == 0)
+sumof_even_fib += newfib;
+}
+printf("%ld\n", sumof_even_fib);
+return (0);
+}
