c9a9b0d01c0fb51c3b838ba83a7f3c28f2f6f6fd
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-16 13:48:28 +0300

    a

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -2,14 +2,10 @@
 /**
 *print_times_table - PRINTS time table of n
 *@n: number
-*
 */
 void print_times_table(int n)
 {
-int j;
-int i;
-int result;
-
+int j, i, result;
 if (n > 15 || n < 0)
 return;
 for (i = 0; i != n + 1; i++)
@@ -17,31 +13,31 @@ for (i = 0; i != n + 1; i++)
 for (j = 0; j != n + 1; j++)
 {
 result = i * j;
-
+dots_result = (1 + i) * (1 + j);
+if (result < 10 && j > 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+}
 if (result < 10)
 _putchar(result % 10 + '0');
-
 if (result >= 10 && result < 100)
 {
+_putchar(' ');
+_putchar(' ');
 _putchar(result / 10 + '0');
 _putchar(result % 10 + '0');
 }
 if (result >= 100)
 {
+_putchar(' ');
 _putchar((result / 100) + '0');
 _putchar((result / 10 % 10) + '0');
 _putchar(result % 10 + '0');
 }
 if (j != n)
-{
 _putchar(',');
-_putchar(' ');
-if(result < 10)
-_putchar(' ');
-if(result < 100)
-_putchar(' ');
-
-}
 }
 _putchar('\n');
 }
