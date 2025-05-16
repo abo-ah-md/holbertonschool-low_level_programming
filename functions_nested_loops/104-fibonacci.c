a5435e2cb74f0cf6ecc96370039e2fb051b689ee
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-16 15:04:36 +0300

    a

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - main function
+*Return: always return 0
+*/
+
+int main(void)
+{
+int i = 1;
+long fib0 = 1;
+long fib1 = 2;
+long newfib = 0;
+printf("%ld, %ld, ", fib0, fib1);
+while (i <= 96)
+{
+newfib = fib0 + fib1;
+if (i == 48)
+printf("%ld", newfib);
+else
+printf("%ld, ", newfib);
+fib0 = fib1;
+fib1 = newfib;
+i++;
+}
+printf("\n");
+return (0);
+}
