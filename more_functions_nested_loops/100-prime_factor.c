03351c36a191036048cb88cbd273711bebe3a418
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-17 21:00:04 +0300

    a

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - check the code
+* Return: Always 0.
+*/
+int main(void)
+{
+int i = 2;
+long number = 612852475143;
+int pf;
+while (i * i < number)
+{
+if (number % i == 0)
+{
+number /= i;
+}
+else
+{
+i++;
+}
+}
+printf("%lu\n", result);
+return (0);
+}
