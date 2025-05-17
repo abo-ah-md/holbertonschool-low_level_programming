4ca5967c700fc285c4bf78f8e51908af00c747f3
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-17 21:19:45 +0300

    a

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_number - prints an intger
+*@n: number
+*/
+void print_number(int n);
+{
+char c;
+if (n < 0)
+{
+_putchar("-");
+n = -n;
+}
+if (n / 10)
+print_number(n / 10);
+c = '0' + (n % 10);
+_putchar(c);
+}
