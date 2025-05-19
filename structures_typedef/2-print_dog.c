902ead847a83e375ba5af4cd0b264187252f71db
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-19 23:55:39 +0300

    a

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+*print_dog -  initialize a variable of type struct dog
+*@d:struct dog
+*/
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return;
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
