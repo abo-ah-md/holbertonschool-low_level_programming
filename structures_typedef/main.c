c7bbfa67a17c8fef3317323244216c0f5f7a0b02
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-19 18:30:01 +0300

    a

--- structures_typedef/main.c
+++ structures_typedef/main.c
@@ -1,33 +1,5 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-
-/**
- * print_grid - prints a grid of integers
- * @grid: the address of the two dimensional grid
- * @width: width of the grid
- * @height: height of the grid
- *
- * Return: Nothing.
- */
-void print_grid(int **grid, int width, int height)
-{
-    int w;
-    int h;
-
-    h = 0;
-    while (h < height)
-    {
-        w = 0;
-        while (w < width)
-        {
-            printf("%d ", grid[h][w]);
-            w++;
-        }
-        printf("\n");
-        h++;
-    }   
-}
+#include "dog.h"
 
 /**
  * main - check the code
@@ -36,18 +8,11 @@ void print_grid(int **grid, int width, int height)
  */
 int main(void)
 {
-    int **grid;
+    struct dog my_dog;
 
-    grid = alloc_grid(6, 4);
-    if (grid == NULL)
-    {
-        return (1);
-    }
-    print_grid(grid, 6, 4);
-    printf("\n");
-    grid[0][3] = 98;
-    grid[3][4] = 402;
-    print_grid(grid, 6, 4);
-    free_grid(grid, 4);
+    my_dog.name = "Poppy";
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
     return (0);
 }
