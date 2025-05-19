74affdabd98c176decab046db6378d02d144da8e
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-19 14:38:52 +0300

    a

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,8 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *str;
-unsigned int j, i = 0;
+unsigned int j = 0;
+unsigned int i = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
