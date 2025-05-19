06a320487484acc8044ff1fe3783b8f6ddb91adf
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-19 23:47:16 +0300

    a

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 /**
 *struct dog - a struct to basic dog info
 *@name: string name of the dog
@@ -9,10 +8,12 @@
 *
 *Description: Holds details about the dog
 **/
+
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
