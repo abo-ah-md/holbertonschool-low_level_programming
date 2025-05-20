d7b6be7d3a9a71cd0294729357e9ceb80c81a4ce
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-20 17:43:47 +0300

    a

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+*free_dog -   frees dogs struct
+*/
+void free_dog(dog_t *d)
+{
+free(d->name);
+free(d->age);
+free(d->owner);
+free(d);
+}
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -4,4 +4,5 @@
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
