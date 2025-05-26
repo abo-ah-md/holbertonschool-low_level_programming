8796a425bd7eede8dddc297b053f6aaccea18802
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-26 14:38:09 +0300

    a

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -5,16 +5,17 @@
 
 int main(int argc, char **argv)
 {
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[3]);
+int num1;
+int num2;
 int (*op_func)(int,int) = get_op_func(argv[2]);
 
-if (argc != 4 || argv[1] == NULL || argv[3] == NULL)
+if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 if (op_func == NULL)
 {
 printf("Error\n");
