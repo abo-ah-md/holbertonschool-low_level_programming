4c59d99da1f06a6b581426f481a1b5afeed7c016
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-26 14:56:49 +0300

    a

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,23 +1,40 @@
 #include "3-calc.h"
+/**
+*op_add - add two int
+*@a:first number
+*@b:second number
+*Return: operation result number
+**/
+int op_add(int a, int b) { return (a + b); }
+/**
+*op_sub - subtract two int
+*@a:first number
+*@b:second number
+*Return: operation result number
+**/
+int op_sub(int a, int b) { return (a - b); }
+/**
+*op_mul - multiply two int
+*@a:first number
+*@b:second number
+*Return: operation result number
+**/
+int op_mul(int a, int b) { return (a * b); }
+/**
+*op_div - devide two int
+*@a:first number
+*@b:second number
+*Return: operation result number
+**/
 
-int op_add(int a, int b)
-{
-return a + b;
-}
-int op_sub(int a, int b)
-{
-return a - b;
-}
-int op_mul(int a, int b)
-{
-return a * b;
-}
-
-int op_div(int a, int b)
-{
-return a / b;
-}
+int op_div(int a, int b) { return (a / b); }
+/**
+*op_mod - mudulate two int
+*@a:first number
+*@b:second number
+*Return: operation result number
+**/
 int op_mod(int a, int b)
 {
-  return a % b;
+return (a % b);
 }
