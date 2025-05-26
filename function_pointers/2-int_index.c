c4932724e0948c5c5faf835beb1e75132cf30630
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-26 11:48:24 +0300

    a

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,12 +11,10 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-int j;
 if (size <= 0)
 return (-1);
 for (i = 0; i < size; i++)
 {
-j = cmp(array[i]);
 if (cmp(array[i]) != 0)
 return (i);
 }
