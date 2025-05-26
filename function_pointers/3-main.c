876ec2ea73b6ea0230a59771bd60aa9553710e51
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-26 14:25:56 +0300

    a

---
+++ function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv)
+{
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[3]);
+int (*op_func)(int,int) = get_op_func(argv[2]);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+if (op_func == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+if ((argv[2][0] == '%' || argv[2][0] == '/') && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+printf("%d\n",op_func(num1,num2));
+return (0);
+}
