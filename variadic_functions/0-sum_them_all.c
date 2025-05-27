2a3d5eca72a7d6cd26470ad10f0b70013832ef1c
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-27 11:49:33 +0300

    a

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,15 +2,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- *sum_them_all- sums all the arguments
- *@n:count of arguments
- * Return: sum of all the numbers
- */
+*sum_them_all- sums all the arguments
+*@n:count of arguments
+* Return: sum of all the numbers
+*/
 int sum_them_all(const unsigned int n, ...)
 {
 va_list args;
 unsigned int sum = 0;
-int i;
+unsigned int i;
 if (n == 0)
 return (0);
 va_start(args, n);
