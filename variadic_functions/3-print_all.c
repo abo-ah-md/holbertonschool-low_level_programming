ff1f5da7837333c1f13fdb42c1fbcfa5669cdc0e
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-29 12:24:47 +0300

    a

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -18,21 +18,25 @@ switch (format[i])
 {
 case 'c':
 printf("%s%c", sep, va_arg(args, int));
+sep = ", ";
 break;
 case 'i':
 printf("%s%d", sep, va_arg(args, int));
+sep = ", ";
 break;
 case 'f':
 printf("%s%f", sep, va_arg(args, double));
+sep = ", ";
 break;
 case 's':
 str = va_arg(args, char *);
 if (str == NULL)
 printf("%s(nil)", sep);
 printf("%s%s", sep, str);
+sep = ", ";
 break;
 }
-sep = ", ";
+
 i++;
 }
 printf("\n");
