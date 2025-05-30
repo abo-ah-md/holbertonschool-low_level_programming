ee0cd83a0925a8ec8cc9718c22bf5f7457b8e4eb
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-30 19:39:55 +0300

    as

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main- main function
+*@argc:number of arguments
+*@argv:argument elemets
+*Return: always 0
+*/
+
+
+int main(int argc, char *argv[])
+{
+int bytes = atoi(argv[1]);
+int i = 0;
+unsigned char *fptr;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+fptr = (unsigned char *) &main;
+
+while (i < bytes)
+{
+printf("%02x", fptr[i]);
+i++;
+}
+printf("\n");
+return (0);
+}
+
