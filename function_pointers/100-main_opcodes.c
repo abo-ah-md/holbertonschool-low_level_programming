daeaa9061c32a0039d7e8c45370cdda45b202c6a
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-30 19:54:13 +0300

    a

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -16,12 +16,12 @@ if (argc != 2)
 printf("Error\n");
 exit(1);
 }
+bytes = atoi(argv[1]);
 if (bytes < 0)
 {
 printf("Error\n");
 exit(2);
 }
-bytes = atoi(argv[1]);
 fptr = (unsigned char *) &main;
 
 while (i < bytes)
