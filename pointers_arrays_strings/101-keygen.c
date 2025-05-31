accdd7bd444cf251539695cc9d4575751025e877
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-31 17:36:33 +0300

    a

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,6 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - check the code
  *
@@ -7,9 +8,29 @@
  */
 int main(void)
 {
-if()
+srand(time(NULL));
+int random;
+int sum = 0;
+char ascii[2780];
+int i = 0;
+while (sum < 2772 - 126)
 {
 
+
+if(sum > 2772)
+{
+while(i > 0)
+ascii[i--]= '\0';
+sum = 0;
+i = 0;
 }
+
+random = rand() % 94 + 33;
+ascii[i++] = (char)random;
+sum += random;
+}
+ascii[i++] = (char)(2772 - sum);
+ascii[i]= '\0';
+printf("%s\n", ascii);
 return (0);
 }
