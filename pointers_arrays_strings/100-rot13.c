5ebd5403a499f469b1512971f178ad40f1f41902
abo-ah-md <abo-ah-md1@hotmail.com>
2025-05-31 23:27:31 +0300

    a

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -5,12 +5,12 @@
 *@str: pointer_number
 *Return:string rot13-string
 */
-char *rot13(char * str)
+char *rot13(char *str)
 {
 int i;
 int j;
-char * alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char * alpha_rot = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char *alpha_rot = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 for (i = 0; str[i]; i++)
 {
 for (j = 0; alpha[j]; j++)
