85d36da017b6d57d3b8b4230b989910126daa108
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-01 16:48:49 +0300

    a

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -6,6 +6,11 @@
 */
 void print_number(int n)
 {
+  if (n < 0) 
+{      
+    n = -n;
+     _putchar('-');
+}
   if (n <= 9 && n >= 0)
 _putchar((n % 10) + '0');    
    if (n >= 10 && n < 100)
@@ -26,28 +31,5 @@ _putchar((n % 10) + '0');
     _putchar((n / 10) % 100 + '0');
     _putchar((n % 10) + '0');
   }
-
-  if (n < 0) {
-    if (n >= -10)
-{      
-    n = -n;
-     _putchar('-');    
-    _putchar((n % 10) + '0');    
-}
-    
-    if (n <= -10 && n > -100)
-{
-n = -n;
-      _putchar('-');
-      _putchar((n / 10) + '0');
-      _putchar((n % 10) + '0');
-    }
-    if (n <= -100) {
-n = -n;      
-      _putchar('-');
-      _putchar((n / 100) + '0');
-      _putchar((n / 10)  % 10 + '0');
-      _putchar((n % 10) + '0');
-    }
   }
-}
+
