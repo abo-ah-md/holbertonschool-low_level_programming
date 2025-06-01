62119a8d60f4be8acf255251006609d271555294
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-01 17:29:23 +0300

    a

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -4,14 +4,14 @@
 *print_number - copies n elements of an arr to another
 *@n: number
 */
-void print_number(int n) {  
+void print_number(int n)
+{  
 unsigned int num = n;
 if (n < 0)
 {
 _putchar('-');
 num = -n;
 }
-
 if (num / 10)
 print_number(num / 10);
 _putchar(num % 10 + '0');
