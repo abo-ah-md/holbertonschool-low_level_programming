c9f1f611ad8258f25c2ed563db3d606edcc0b1f7
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-05 22:56:26 +0300

    formatted for cleaner code

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,13 +15,14 @@ list_t *new_node;
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 return (NULL);
-for(len = 0; str[len];len++);
+for (len = 0; str[len]; len++)
+;
 new_node->len = len;
 new_node->str = strdup(str);
-if(new_node->str == NULL)
+if (new_node->str == NULL)
 {
 free(new_node);
-return NULL;
+return (NULL);
 }
 new_node->next = *head;
 *head = new_node;
