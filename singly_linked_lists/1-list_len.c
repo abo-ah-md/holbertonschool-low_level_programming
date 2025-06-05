e3988fcbdecbefc95b217a12a41d133aa520d173
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-05 22:53:29 +0300

    added function for adding nodes to a linkedlist

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,34 +2,6 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
-*print_str - prints string
-*@str: string
-*/
-void print_str(char *str)
-{
-int i = 0;
-while (str[i])
-{
-_putchar(str[i]);
-i++;
-}
-}
-/**
-*print_num - prints numbers
-*@num: number
-*/
-void print_num(int num)
-{
-if (num < 0)
-{
-_putchar('-');
-num = -num;
-}
-if (num / 10)
-print_num(num / 10);
-_putchar((num % 10) + '0');
-}
-/**
 *list_len - prints the elements of a nodes
 *@h: node struct
 *Return: number of pointers
