bd0940a09e53c779ecf1404f1b47d7f1bfc1ac0f
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-17 22:14:42 +0300

    added newline to stdout

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 int number_of_nodes;
 number_of_nodes = 0;
 while (h != NULL) {
-printf("%d", h->n);
+printf("%d\n", h->n);
 h = h->next;  
 number_of_nodes++;
 }
