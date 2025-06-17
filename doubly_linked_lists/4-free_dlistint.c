8f9a6f7d8444f6d1a6f4180c6e244a6966b1802b
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-18 02:25:04 +0300

    added free nodes function

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,59 +1,16 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 /**
-*print_str - prints string
-*@str: string
+*free_dlistint - adds a node to the list end
+*@head:node pointer
 */
-void print_str(char *str)
+void free_dlistint(dlistint_t *head)
 {
-int i = 0;
-while (str[i])
-{
-_putchar(str[i]);
-i++;
-}
-}
-/**
-*print_num - prints numbers
-*@num: number
-*/
-void print_num(int num)
-{
-if (num < 0)
-{
-_putchar('-');
-num = -num;
-}
-if (num / 10)
-print_num(num / 10);
-_putchar((num % 10) + '0');
-}
-/**
-*print_list - prints the elements of a nodes
-*@h: node struct
-*Return: number of pointers
-*/
-size_t print_list(const list_t *h)
-{
-int i = 0;
-while (h != NULL)
-{
-_putchar('[');
-print_num(h->len);
-_putchar(']');
-_putchar(' ');
-if (h->str == NULL)
-{
-print_str("(nil)\n");
-}
-else
-{
-print_str(h->str);
-_putchar('\n');
-}
-i++;
-h = h->next;
-}
-return (i);
+dlistint_t *temp;
+while (head != NULL)
+temp = head->next;
+free(head);
+head = temp;
 }
