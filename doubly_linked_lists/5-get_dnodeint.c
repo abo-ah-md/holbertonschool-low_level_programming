46d76370f9831becf3fc88a302dd18a5c9dd3249
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-18 02:42:27 +0300

    adding function getting nth node using index

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,59 +1,24 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 /**
-*print_str - prints string
-*@str: string
+*get_dnodeint_at_index - gets node index
+*@head:node pointer
+*@index: index of the node
+*Return:the nth node of index
 */
-void print_str(char *str)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-int i = 0;
-while (str[i])
+unsigned int i;
+i = 0;
+while (head != NULL)
 {
-_putchar(str[i]);
+if (i == index)
+return (head);
 i++;
+head = head->next;
 }
-}
-/**
-*print_num - prints numbers
-*@num: number
-*/
-void print_num(int num)
-{
-if (num < 0)
-{
-_putchar('-');
-num = -num;
-}
-if (num / 10)
-print_num(num / 10);
-_putchar((num % 10) + '0');
-}
-/**
-*print_list - prints the elements of a nodes
-*@h: node struct
-*Return: number of pointers
-*/
-size_t print_list(const list_t *h)
-{
-int i = 0;
-while (h != NULL)
-{
-_putchar('[');
-print_num(h->len);
-_putchar(']');
-_putchar(' ');
-if (h->str == NULL)
-{
-print_str("(nil)\n");
-}
-else
-{
-print_str(h->str);
-_putchar('\n');
-}
-i++;
-h = h->next;
-}
-return (i);
+
+return (NULL);
 }
