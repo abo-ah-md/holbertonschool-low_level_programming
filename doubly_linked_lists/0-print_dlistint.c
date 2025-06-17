ac6e814839b093e2d351c993969673655530aeeb
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-17 22:12:08 +0300

    added function of printing elements on doubly linked list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,59 +1,19 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 /**
-*print_str - prints string
-*@str: string
+*print_dlistint - prints
+*@h: string
 */
-void print_str(char *str)
+size_t print_dlistint(const dlistint_t *h)
 {
-int i = 0;
-while (str[i])
-{
-_putchar(str[i]);
-i++;
+int number_of_nodes;
+number_of_nodes = 0;
+while (h != NULL) {
+printf("%d", h->n);
+h = h->next;  
+number_of_nodes++;
 }
-}
-/**
-*print_num - prints numbers
-*@num: number
-*/
-void print_num(int num)
-{
-if (num < 0)
-{
-_putchar('-');
-num = -num;
-}
-if (num / 10)
-print_num(num / 10);
-_putchar((num % 10) + '0');
-}
-/**
-*print_list - prints the elements of a nodes
-*@h: node struct
-*Return: number of pointers
-*/
-size_t print_list(const list_t *h)
-{
-int i = 0;
-while (h != NULL)
-{
-_putchar('[');
-print_num(h->len);
-_putchar(']');
-_putchar(' ');
-if (h->str == NULL)
-{
-print_str("(nil)\n");
-}
-else
-{
-print_str(h->str);
-_putchar('\n');
-}
-i++;
-h = h->next;
-}
-return (i);
+return (number_of_nodes);
 }
