6546eea64232644555bfe85fa0338f64cd27607b
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-17 22:18:25 +0300

    fixed bitty style

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,8 +3,9 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
-*print_dlistint - prints
-*@h: string
+*print_dlistint - prints doubly linked list elements
+*@h:string
+*Return: number of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
@@ -13,7 +14,7 @@ number_of_nodes = 0;
 while (h != NULL)
 {
 printf("%d\n", h->n);
-h = h->next;  
+h = h->next;
 number_of_nodes++;
 }
 return (number_of_nodes);
