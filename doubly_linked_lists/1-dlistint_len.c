55b3f3724b8176e157490e1ae5af8c6080e4c68f
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-17 22:21:45 +0300

    added function returnning number of elements

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,59 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>                                                                                     ### stddef.h  include
+#include <stdlib.h>                                                                                     ### stddef.h  stddef
+#include "lists.h"                                                                                           ### stddef.h  h
 /**
-*print_str - prints string
-*@str: string
+*print_dlistint - prints doubly linked list elements
+*@h:string
+*Return: number of nodes
 */
-void print_str(char *str)
+size_t dlistint_len(const dlistint_t *h)
 {
-int i = 0;
-while (str[i])
-{
-_putchar(str[i]);
-i++;
-}
-}
-/**
-*print_num - prints numbers
-*@num: number
-*/
-void print_num(int num)
-{
-if (num < 0)
-{
-_putchar('-');
-num = -num;
-}
-if (num / 10)
-print_num(num / 10);
-_putchar((num % 10) + '0');
-}
-/**
-*print_list - prints the elements of a nodes
-*@h: node struct
-*Return: number of pointers
-*/
-size_t print_list(const list_t *h)
-{
-int i = 0;
+int number_of_nodes;
+number_of_nodes = 0;
 while (h != NULL)
 {
-_putchar('[');
-print_num(h->len);
-_putchar(']');
-_putchar(' ');
-if (h->str == NULL)
-{
-print_str("(nil)\n");
-}
-else
-{
-print_str(h->str);
-_putchar('\n');
-}
-i++;
 h = h->next;
+number_of_nodes++;
 }
-return (i);
+return (number_of_nodes);
 }
