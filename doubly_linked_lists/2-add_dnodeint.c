f83197aca00d249555ac87f9025aabea0b91d491
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-18 00:49:14 +0300

    added (add node to list beginning)

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,59 +1,26 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 /**
-*print_str - prints string
-*@str: string
+*add_dnodeint - prints doubly linked list elements
+*@head:node pointer
+*Return: number of nodes
 */
-void print_str(char *str)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-int i = 0;
-while (str[i])
+dlistint_t *new_node;  
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return NULL;
+new_node->n = n;
+new_node->next = *head;
+new_node->prev = NULL;
+if (*head == NULL)
 {
-_putchar(str[i]);
-i++;
+*head = new_node;
+return (new_node);
 }
-}
-/**
-*print_num - prints numbers
-*@num: number
-*/
-void print_num(int num)
-{
-if (num < 0)
-{
-_putchar('-');
-num = -num;
-}
-if (num / 10)
-print_num(num / 10);
-_putchar((num % 10) + '0');
-}
-/**
-*print_list - prints the elements of a nodes
-*@h: node struct
-*Return: number of pointers
-*/
-size_t print_list(const list_t *h)
-{
-int i = 0;
-while (h != NULL)
-{
-_putchar('[');
-print_num(h->len);
-_putchar(']');
-_putchar(' ');
-if (h->str == NULL)
-{
-print_str("(nil)\n");
-}
-else
-{
-print_str(h->str);
-_putchar('\n');
-}
-i++;
-h = h->next;
-}
-return (i);
+(*head)->prev = new_node;
+return (new_node);
 }
