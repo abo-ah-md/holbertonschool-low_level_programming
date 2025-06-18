6649fcaf58928d24e4859959ffeb7e92988850f9
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-19 01:55:04 +0300

    a

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -60,7 +60,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
         check->prev = new_node;
         return new_node;
     }
-    else // Insert at end
+    else
     {
         dlistint_t *tail = *h;
         while (tail && tail->next)
