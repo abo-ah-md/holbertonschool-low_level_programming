6f14757b8aa7e02ad0c4b8d2acf498cbb1fdb4c3
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-19 01:52:16 +0300

    a

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,59 +11,69 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node, *check;
-unsigned int i;
-i = 1;
-new_node = malloc(sizeof(dlistint_t));
-if(new_node == NULL)
-return NULL;
-new_node->n = n;
-check = *h;
-if(idx == 0)
-{
-new_node->prev = NULL;
-new_node->next = check;
-if (check != NULL)
-check->prev = new_node;
-*h = new_node;
-return (new_node);
-}
-/**
-if(check->next == NULL && idx == 1)
-return NULL;
-**/
-if(check == NULL)
-check= *h;
-while(check->next != NULL)
-{
-check = check->next;
-if(i == idx)
-break;
-i++;
-}
-if(i == idx && check->next != NULL)
-{
-new_node->next = check;
-new_node->prev = check->prev;
- if (check->prev)
-        check->prev->next = new_node;
-    else
+    dlistint_t *new_node, *check;
+    unsigned int i = 0;
+
+    if (!h)
+        return (NULL);
+
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = n;
+    new_node->prev = NULL;
+    new_node->next = NULL;
+    check = *h;
+
+    if (idx == 0)
+    {
+        new_node->next = *h;
+        if (*h)
+            (*h)->prev = new_node;
         *h = new_node;
+        return (new_node);
+    }
 
-    check->prev = new_node;
-    return (new_node);
-}
+    while (check && i < idx)
+    {
+        check = check->next;
+        i++;
+    }
 
-if (idx > i)
-{
-return(NULL);
-}
-if(check->next == NULL)
-{
-new_node->next = NULL;
-new_node->prev = check;
-check->next = new_node;
-return new_node;
-}
-return new_node;
+    if (i != idx)
+    {
+        free(new_node);
+        return (NULL); // Index out of range
+    }
+
+    if (check) // Insert before check
+    {
+        new_node->next = check;
+        new_node->prev = check->prev;
+
+        if (check->prev)
+            check->prev->next = new_node;
+        else
+            *h = new_node;
+
+        check->prev = new_node;
+        return new_node;
+    }
+    else // Insert at end
+    {
+        dlistint_t *tail = *h;
+        while (tail && tail->next)
+            tail = tail->next;
+
+        if (tail)
+        {
+            tail->next = new_node;
+            new_node->prev = tail;
+        }
+        else
+            *h = new_node;
+
+        return new_node;
+    }
 }
