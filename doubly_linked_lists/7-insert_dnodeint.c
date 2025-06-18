0f93895fbf28ef3b8636393b42da2399e872603c
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-18 17:05:07 +0300

    fixed reordering of node insertion

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,19 +13,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *new_node, *check;
 unsigned int i;
-
 if (*h == NULL)
 return (NULL);
-
 check = *h;
 i = 0;
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
-
 new_node->n = n;
-
-
 if (idx == 0)
 {
 new_node->prev = NULL;
@@ -33,24 +28,20 @@ new_node->next = check;
 *h = new_node;
 return (new_node);
 }
-
-while (check->next != NULL)
+while (check->next != NULL && i < idx)
 {
 check = check->next;
 i++;
 }
-
-
-if (check->next  == NULL)
+if (idx > i)
+return (NULL);
+if (i == idx && check->next == NULL)
 {
 new_node->next = NULL;
 new_node->prev = check;
 check->next = new_node;
-
 return (new_node);
 }
-
-
 new_node->prev = check->prev;
 check->prev->next = new_node;
 check->prev = new_node;
