d63df112f916052527d6b330a05424b77ed2adf6
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-22 23:19:42 +0300

    added a C function to get required bit-num to flip to get value of other number

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*flip_bits - get requried bit to flip to get from n to m
+*@n:bit source
+*@m:bit distanation
+*Return:returns the number of bits needed to flip to get from one n to m
+**/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int xor = n ^ m;
+unsigned int count = 0;
+while (xor)
+{
+count += xor & 1;
+xor >>= 1;
+}
+
+return count;
+}
