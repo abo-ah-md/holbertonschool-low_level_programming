1c62215d38b1dc1c636829f4579f48b0e1c67ccc
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-22 22:18:58 +0300

    added a function to get the bit value of a decmal number on geven index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+*get_bit - converts binary to unsigned int
+*@n:binary in string format
+*@index:binary in string format
+*Return:value of the bit at index
+*index or -1 if an error occured
+**/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int i;
+unsigned long int mask;
+int started;
+started = 0;
+if (n == 0)
+return (0);
+for (i = 63; i != (unsigned int)-1; i--)
+{
+mask = 1UL << i;
+if ((n & mask) != 0)
+started = 1;
+if (started)
+{
+if ((n & mask))
+{
+if (i == index)
+return (1);
+}
+else
+{
+if (i == index)
+return (0);
+}
+}
+}
+}
