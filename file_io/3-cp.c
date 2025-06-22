a4f06604b502ccdd90223ee7bf341f406be99370
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-23 02:55:52 +0300

    .

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -25,7 +25,7 @@ fd1 = open(argv[1], O_RDONLY);
 if (fd1 == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+exit(99);
 }
 fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,0664);
 if (fd2 == -1)
