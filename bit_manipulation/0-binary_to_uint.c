f0a95ec84020a637e10ce751bd0d7e7406c892df
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-22 13:28:00 +0300

    fixed betty style mixed declaration c89

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,12 +9,13 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-if (b == NULL)
-return (0)
 int i;
 unsigned int result;
 result = 0;
 i = 0;
+if (b == NULL)
+return (0)
+
 while (b[i] != '\0')
 {
 if (b[i] != '0' && b[i] != '1')
