edbd28ca75fe4314921d6897aed3bacac1ce5bed
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-22 22:57:38 +0300

    fixed the issue of out of range index

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
-*get_bit - converts binary to unsigned int
+*get_bit - get the value of bit index of n
 *@n:binary in string format
 *@index:binary in string format
 *Return:value of the bit at index
@@ -12,43 +12,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-unsigned int i;
-unsigned int j;
-unsigned long int mask;
-int started;
-int *num_arr;
-
-num_arr = malloc(sizeof(int) * 20);
-if (!num_arr)
-exit(EXIT_FAILURE);
-j = 0;
-started = 0;
-if (n == 0)
-return (0);
-
-for (i = 63; i != (unsigned int)-1; i--)
-{
-mask = 1UL << i;
-if ((n & mask) != 0)
-started = 1;
-if (started)
-{
-if ((n & mask))
-{
-num_arr[j] = 1;
-j++;
-}
-else
-{
-num_arr[j] = 0;
-j++;
-}
-}
-}
-if (index > j)
+if (index >= 64)
 return (-1);
-
-return (num_arr[index]);
-
-return (-1);
+return ((n >> index) & 1);
 }
