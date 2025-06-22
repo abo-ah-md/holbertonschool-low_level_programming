e03d04c607afbc4eaf933130f73b4d912ef80d0f
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-23 01:11:08 +0300

    fixed null text case

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,12 +17,20 @@ int create_file(const char *filename, char *text_content)
 {
 int fd;
 ssize_t file_write;
-if (!filename || !text_content)
+if (!filename)
 return (-1);
 
 fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
+
+if (!text_content)
+{
+close(fd);
+return (1);
+}
+
+
 
 file_write = write(fd, text_content, strlen(text_content));
 if (file_write == -1)
